1e7cf2d619f1c2c673d70b6be754417f09f778bf
Rajdeep Dua <rdua@salesforce.com>
2020-05-16 17:59:14 +0530

    created  new app

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.POST("/api", handleVerification)
+
+	router.POST("/login", func(c *gin.Context) {
+		//What do I need to put here?
+		//formContent := c.PostForm("loginForm")
+		emailValue := c.PostForm("email");
+		passwordValue := c.PostForm("password");
+		c.HTML(http.StatusOK, "result.tmpl.html",
+			gin.H{
+				"email": emailValue,
+				"password":passwordValue,
+			})
+		/*
+		c.JSON(200, gin.H{
+			"status":  "posted to login",
+			"message": "whoo",
+			"email": emailValue,
+		    "password": passwordValue})*/
+	})
+
+	router.POST("/signedrequest", func(c *gin.Context) {
+		//What do I need to put here?
+		//formContent := c.PostForm("loginForm")
+		//emailValue := c.PostForm("email")
+		//passwordValue := c.PostForm("password");
+		signedRequest := c.PostForm("signed_request")
+		c.HTML(http.StatusOK, "result_sr.tmpl.html",
+			gin.H{
+				"signed_request": signedRequest,
+			})
+		/*
+			c.JSON(200, gin.H{
+				"status":  "posted to login",
+				"message": "whoo",
+				"email": emailValue,
+			    "password": passwordValue})*/
+	})
+	router.Run(":" + port)
+}
+func handleVerification(c *gin.Context) {
+	if c.Request.Method == "POST" {
+
+		c.Request.ParseForm()
+		param1 := c.Request.PostForm["param1"]
+		log.Printf("%v",c.Request.PostForm["param1"]) // ""
+		c.HTML(http.StatusOK, "index.tmpl.html",
+			gin.H{
+				"param1": param1,
+			})
+	}
+}
